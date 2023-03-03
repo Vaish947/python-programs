0e0c34bd74daff8124d24c01926a651c8ada1f0f
Vaish947 <vaishnavisonne@gmail.com>
2023-03-04 05:10:17 +0530

    for_loop_programs

---
+++ for_loop_programs/break_continuous_pass.py
@@ -0,0 +1,17 @@
+for i in range(1,5):
+    if i==3:
+        break
+    print(i)
+##########################
+for i in range(1,5):
+    if i==3:
+        continue
+    print(i)
+#############################
+for i in range(1,5):
+    pass
+class test:
+    pass
+
+
+
---
+++ for_loop_programs/range_function.py
@@ -0,0 +1,21 @@
+s1='hello'
+arr=[10,20,30,40,50]
+for y in arr:
+    print(y*y)
+for x in s1:
+    print(x)
+######################
+for i in range(1,10,1):
+    print(i)
+##########################
+for i in range(10):
+    print(i)
+#########################
+for i in range(1,10):
+    print(i)
+#########################
+for i in range(1,10,2):
+    print(i)
+##########################
+for i in range(10,0,-1):
+    print(i)
